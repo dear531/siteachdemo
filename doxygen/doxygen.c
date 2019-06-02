2227a952b01447f9ed017127be08235f37b92cf8
zhangly <zhangly@localhost.localdomain>
2019-06-03 05:56:18 +0800

    init doxygen

---
+++ doxygen/doxygen.c
@@ -0,0 +1,14 @@
+/**
+ * @file doxygen.c
+ * @brief doxygen command line test 
+ * @author Leo chueng
+ * @version [doxygen 1.0]
+ * @date 2019.6.2pm14:30
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	fprintf(stdout, "hello word\n");
+	return 0;
+}
