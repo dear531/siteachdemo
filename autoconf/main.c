6166859681e0419649bfdc0a39aca8b5bd2f650a
Leo <dear531@21cn.com>
2019-06-08 23:34:12 +0800

    add seting CPPFLAGS

--- autoconf/main.c
+++ autoconf/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 #include "add.h"
 
 int main(void)
 {
 	int m = 1, n = 2;
+	double d = 5.0;
 	fprintf(stdout, "hello world\n");
 	fprintf(stdout, "%d + %d = %d\n", m, n, add(m, n));
+	fprintf(stdout, "log(%lf):%lf\n", d, log(d));
 	return 0;
 }
